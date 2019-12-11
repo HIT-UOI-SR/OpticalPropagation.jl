368d0acd0c0735aa37aee5ad1691bd914aee493f
miRoox <miroox@outlook.com>
2019-12-11 15:40:32 +0800

    fix documenter assets

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,13 @@ using Documenter, LightPropagation
 
 makedocs(;
     modules=[LightPropagation],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(assets=String[]),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/miRoox/LightPropagation.jl/blob/{commit}{path}#L{line}",
     sitename="LightPropagation.jl",
     authors="miRoox",
-    assets=String[],
 )
 
 deploydocs(;
