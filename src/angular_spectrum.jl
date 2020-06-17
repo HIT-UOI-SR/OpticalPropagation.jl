4150779616c70fdc0c791f1e7e9c2114b7a792f2
miroox <miroox@outlook.com>
2020-06-17 16:54:24 +0800

    rename parameters

--- src/angular_spectrum.jl
+++ src/angular_spectrum.jl
@@ -1,11 +1,11 @@
 """
-    angular_spectrum(data, d, λ, lx, ly)
+    angular_spectrum(Uin, d, λ, lx, ly)
 
 calculate the propagation light field based on the angular spectrum.
 """
-function angular_spectrum(data::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
+function angular_spectrum(Uin::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
     tf = (u, v) -> 1-u^2-v^2>=0 ? exp(2im*pi*d/λ*sqrt(1-u^2-v^2)) : 0
-    (n, m) = size(data)
+    (n, m) = size(Uin)
     (u, v) = (-n/2+1:n/2,-m/2+1:m/2)./(lx,ly).*λ
-    ifft(fft(data).*fftshift(tf.(u',v)))
+    ifft(fft(Uin).*fftshift(tf.(u',v)))
 end
--- src/fresnel_diffraction.jl
+++ src/fresnel_diffraction.jl
@@ -1,28 +1,28 @@
 """
-    fresnel_diffraction_double(data, d, λ, lx, ly)
+    fresnel_diffraction_double(Uin, d, λ, lx, ly)
 
 calculate the propagation light field based on the Fresnel diffraction with double Fourier transform.
 """
-function fresnel_diffraction_double(data::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
+function fresnel_diffraction_double(Uin::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
     tf = (u, v) -> exp(2im*pi*d/λ)*exp(-im*pi*d/λ*(u^2+v^2))
-    (n, m) = size(data)
+    (n, m) = size(Uin)
     (u, v) = (-n/2+1:n/2,-m/2+1:m/2)./(lx,ly).*λ
-    ifft(fft(data).*fftshift(tf.(u',v)))
+    ifft(fft(Uin).*fftshift(tf.(u',v)))
 end
 
 """
-    fresnel_diffraction_single(data, d, λ, lx, ly)
+    fresnel_diffraction_single(Uin, d, λ, lx, ly)
 
 calculate the propagation light field based on the Fresnel diffraction with single Fourier transform.
 """
-function fresnel_diffraction_single(data::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
+function fresnel_diffraction_single(Uin::AbstractArray{<:Number,2}, d::Real, λ::Real, lx::Real, ly::Real)
     qp = (x, y) -> exp(im*pi/d/λ*(x^2+y^2))
-    (n, m) = size(data)
+    (n, m) = size(Uin)
     (dx, dy) = (lx, ly)./(n, m)
     (lxo, lyo) = λ*d./(dx, dy)
     (u, v) = (-n/2+1:n/2,-m/2+1:m/2)./(lx,ly).*λ
-    amp = fft(data.*qp.(u', v))
+    amp = fft(Uin.*qp.(u', v))
     (uo, vo) = (-n/2+1:n/2,-m/2+1:m/2)./(lxo,lyo).*λ
-    patt = amp.*qp.(u', v).*exp(2im*pi*d/λ).*dx.*dy./(im*λ*d)
-    (patt, (lxo, lyo))
+    Uout = amp.*qp.(u', v).*exp(2im*pi*d/λ).*dx.*dy./(im*λ*d)
+    (Uout, (lxo, lyo))
 end
